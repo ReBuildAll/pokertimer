dce62cf38e2d0aa67b5997d8c4509b3c34bdf3a6
Lenard Gunda <lenard@offbeat.fi>
2016-02-08 22:33:39 +0200

    Fixed deploy command for kudu

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,9 @@
 @echo off
-npm install
-bower install
-gulp build --environment Production
+echo "NPM"
+call npm install
+echo "BOWER"
+call bower install
+echo "Generate static file"
+call gulp build --environment Production
+echo "Deploy"
 xcopy %DEPLOYMENT_SOURCE%\dist %DEPLOYMENT_TARGET% /Y /E
