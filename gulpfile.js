835c38d8db726cbd5317d8a4c4a5b3c0e6b5ad1e
Lenard Gunda <lenard@offbeat.fi>
2016-02-08 22:20:04 +0200

    Support for "production" and "development" builds, no appcache by default

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var sourcemaps = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var rename = require("gulp-rename");
 var minifyCSS = require("gulp-minify-css");
+var argv = require('yargs').argv;
 
 var paths = {
   "staticfiles": "static/**/",
@@ -20,6 +21,25 @@ var paths = {
   "dest": "dist/",  
   "destCss": "dist/css/",
   "destJs": "dist/js/"
+};
+
+var environment = {
+	// The names of the different environments.
+	development: "Development",
+	staging: "Staging",
+	production: "Production",
+	// Gets the current hosting environment the application is running under. This comes from the environment variables.
+	current: function () {
+        return argv.environment
+        || process.env["Hosting:Environment"] 
+        || this.development 
+    },
+	// Are we running under the development environment.
+	isDevelopment: function () { return this.current() === this.development; },
+	// Are we running under the staging environment.
+	isStaging: function () { return this.current() === this.staging; },
+	// Are we running under the production environment.
+	isProduction: function () { return this.current() === this.production; }
 };
 
 gulp.task("clean", function (cb) {
@@ -33,7 +53,13 @@ gulp.task("staticfiles", function() {
 
 gulp.task('html', [], function() {
     gulp.src(path.join(paths.html, "*.html"))   
-        .pipe(fileinclude())
+        .pipe(fileinclude({
+            context: {
+                isProduction: environment.isProduction(),
+                isStatging: environment.isStaging(),
+                isDevelopment: environment.isDevelopment()
+            }
+        }))
         .pipe(gulp.dest(paths.dest));
 });
 
@@ -74,7 +100,7 @@ gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9001,
+    port: 9002,
     server: {
       baseDir: ['./dist'],
       middleware: function(req, res, next) {
@@ -85,45 +111,3 @@ gulp.task('serve', ['build'], function(done) {
   }, done);
 });
 
-// var gulp = require("gulp"),
-//     rimraf = require("rimraf"),
-//     concat = require("gulp-concat"),
-//     cssmin = require("gulp-cssmin"),
-//     uglify = require("gulp-uglify");
-// 
-// var paths = {
-//     webroot: "./wwwroot/"
-// };
-// 
-// paths.js = paths.webroot + "js/**/*.js";
-// paths.minJs = paths.webroot + "js/**/*.min.js";
-// paths.css = paths.webroot + "css/**/*.css";
-// paths.minCss = paths.webroot + "css/**/*.min.css";
-// paths.concatJsDest = paths.webroot + "js/site.min.js";
-// paths.concatCssDest = paths.webroot + "css/site.min.css";
-// 
-// gulp.task("clean:js", function (cb) {
-//     rimraf(paths.concatJsDest, cb);
-// });
-// 
-// gulp.task("clean:css", function (cb) {
-//     rimraf(paths.concatCssDest, cb);
-// });
-// 
-// gulp.task("clean", ["clean:js", "clean:css"]);
-// 
-// gulp.task("min:js", function () {
-//     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-//         .pipe(concat(paths.concatJsDest))
-//         .pipe(uglify())
-//         .pipe(gulp.dest("."));
-// });
-// 
-// gulp.task("min:css", function () {
-//     return gulp.src([paths.css, "!" + paths.minCss])
-//         .pipe(concat(paths.concatCssDest))
-//         .pipe(cssmin())
-//         .pipe(gulp.dest("."));
-// });
-// 
-// gulp.task("min", ["min:js", "min:css"]);
